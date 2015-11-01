3f10850365eb2273d83250f17f15358898600c66
Gummi <gudmundurk14@ru.is>
2015-11-01 17:26:54 +0000

    Changed Gameboard class like we talked about. Moved win condition logic to game class

--- src/main/java/is/lusifer/tictactoe/GameBoard.java
+++ src/main/java/is/lusifer/tictactoe/GameBoard.java
@@ -1,39 +1,34 @@
 package is.lusifer.tictactoe;
 
 public class GameBoard {
-   private static final int boardSize = 9;
-   private int[] board = new int[boardSize];
+    private char board[];
+    private int cellsFull;
 
-   public void set (int tile, int player) {
-      if (tile > 8 || tile < 0) {
-         throw new IllegalArgumentException("Input needs to be between 0 and 8");
-      }
-      board[tile] = player;
-   }
+    public GameBoard() {
+        board = new char[9];
+        for(int i = 0; i < 9; i++)
+            board[i] = ' ';
+        cellsFull = 0;
+    }
 
-   public boolean tileStreak(int a, int b, int c) {
-      int x = board[a];
-      int y = board[b];
-      int z = board[c];
-      return x != 0 && x == y && y == z;
-   }
+    public char[] getBoard() {
+        return board;
+    }
 
-   // checks if there is already a winner
-   public boolean someoneWon() {
-      return (tileStreak(0,1,2) || tileStreak(3,4,5)
-         || tileStreak(6,7,8) || tileStreak(0,3,6) 
-         || tileStreak(1,4,7) || tileStreak(2,5,8)
-         || tileStreak(0,4,8) || tileStreak(2,4,6)
-      );
-   }
+    public void makeMove(int tile, char mark) {
+        board[tile] = mark;
+        cellsFull++;
+    }
 
-   // checks each tile and looks for one that doesnt have an entry
-   public boolean isBoardFull () {
-      for (int i = 0; i < boardSize; i++) {
-         if (board[i] == 0) {
-            return false;
-         }
-      }
-      return true;
-   }
+    public char getCell(int tile) {
+        return board[tile];
+    }
+
+    public boolean isCellEmpty(int tile) {
+        return board[tile] == ' ';
+    }
+
+    public boolean isBoardFull() {
+        return cellsFull >= 9;
+    }
 }
---
+++ src/test/java/is/lusifer/tictactoe/GameBoardTests.java
@@ -0,0 +1,60 @@
+package is.lusifer.tictactoe;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GameBoardTests {
+    public static void main(String args[]) {
+        org.junit.runner.JUnitCore.main("Tests.PlayerTests");
+    }
+
+    @Test
+    public void testBoardCreation() {
+        GameBoard board = new GameBoard();
+        char emptyTile = ' ';
+        for (char tile : board.getBoard()) {
+            assertEquals(emptyTile, tile);
+        }
+    }
+
+    @Test
+    public void testMakeMove() {
+        GameBoard board = new GameBoard();
+        char testChar = 'X';
+        board.makeMove(0, testChar);
+        char testArr[] = board.getBoard();
+        assertEquals(testChar, testArr[0]);
+    }
+
+    @Test
+    public void testIsBoardFull() {
+        GameBoard board = new GameBoard();
+        board.makeMove(0, 'X');
+        board.makeMove(1, 'X');
+        board.makeMove(2, 'X');
+        board.makeMove(3, 'X');
+        board.makeMove(4, 'X');
+        board.makeMove(5, 'X');
+        board.makeMove(6, 'X');
+        board.makeMove(7, 'X');
+        assertEquals(false, board.isBoardFull());
+        board.makeMove(8, 'X');
+        assertTrue(board.isBoardFull());
+    }
+
+    @Test
+    public void testIsCellEmpty() {
+        GameBoard board = new GameBoard();
+        board.makeMove(0, 'X');
+        assertEquals(true, board.isCellEmpty(1));
+        assertEquals(false, board.isCellEmpty(0));
+    }
+
+    @Test
+    public void testGetCell() {
+        GameBoard board = new GameBoard();
+        board.makeMove(0, 'X');
+        assertEquals(' ', board.getCell(1));
+        assertEquals('X', board.getCell(0));
+    }
+}
