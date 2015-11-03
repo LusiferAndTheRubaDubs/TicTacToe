2a81069b4075d52ef8d7a7404e2d093c7fe3d693
Gummi <gudmundurk14@ru.is>
2015-11-03 00:11:33 +0000

    playerMove half finished

--- src/main/java/is/lusifer/tictactoe/WebUI.java
+++ src/main/java/is/lusifer/tictactoe/WebUI.java
@@ -5,6 +5,8 @@ import static spark.Spark.*;
 import spark.servlet.SparkApplication;
 import is.lusifer.tictactoe.Logic.TicTacToeGame;
 import is.lusifer.tictactoe.Domain.Player;
+import is.lusifer.tictactoe.Logic.InvalidMoveException;
+import com.google.gson.Gson;
 
 public class WebUI implements SparkApplication{
     public static void main(String[] args) {
@@ -22,7 +24,16 @@ public class WebUI implements SparkApplication{
     @Override
     public void init() {
         final TicTacToeGame game = new TicTacToeGame(new Player(), new Player());    
-		get("/test", (req, res) -> "Hello World");	
+		post("/playerMove", (req, res) -> {
+			System.out.println(req);
+			try {
+				game.makeMove(Integer.parseInt(req.queryParams("tile")));  
+			}catch(InvalidMoveException e) {
+				res.status(500);
+				return res;
+			}
+            return new Gson().toJson(game.getGameBoard());
+        });
     }
 
 }
--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-            alert("works");
-        });
+    alert("works");
+	$(".tile").click(function () {
+        var tile = (this).value;
+		var button = (this);
+		alert(tile);
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:4567/playerMove",
+			traditional: true,
+			data: 'tile=' + tile,
+			success: function (data) {
+				obj = JSON.parse(data);
+				alert(obj[tile]);
+				button.innerHTML = obj[tile];
+			},
+			error: function (data) {
+				alert("error");
+			}
+		});
+    });
+});
+
+function makePlay(type){
+
+};
